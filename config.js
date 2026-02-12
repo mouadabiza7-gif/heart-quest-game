82fcc287afb89a75fe540148cabb119c374fb7e7
mouadabiza7-gif <mouadabiza7@gmail.com>
2026-02-12 01:14:16 +0100

    Create config.js with game configuration

---
+++ config.js
@@ -0,0 +1,20 @@
+const gameConfig = {
+    playerName: "YourPlayerName",
+    valentinesName: "ValentinesName",
+    customMessages: [
+        "Message 1",
+        "Message 2",
+        "Message 3"
+    ],
+    gameSettings: {
+        difficulty: "normal", // Options: easy, normal, hard
+        sound: true,
+        music: true,
+    },
+    customizationOptions: {
+        background: "default",
+        character: "default",
+    }
+};
+
+module.exports = gameConfig;
